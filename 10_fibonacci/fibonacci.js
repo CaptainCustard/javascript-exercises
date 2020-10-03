d3cf22276febd069589b094e0f0721d801329599
Omar Saleh <omarsaleh92@gmail.com>
2020-10-03 18:04:21 -0500

    Adding solutions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
-}
-
+const fibonacci = function(num) {
+    let series = [1, 1];
+    if (num >= 0) {
+        for (let i = 2; i <= num; i++) {
+            series [i] = series [i - 1] + series [i-2];
+        }
+        return series[num-1]
+    } else {
+        return 'OOPS'
+    }  
+} 
+// console.log(fibonacci(5))
 module.exports = fibonacci
