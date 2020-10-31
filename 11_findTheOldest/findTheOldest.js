0efb63fa0da8ff2bca2096f08d4092949bf26b00
Omar Saleh <omarsaleh92@gmail.com>
2020-10-31 16:31:44 -0500

    Solved

--- 11_findTheOldest/findTheOldest.js
+++ 11_findTheOldest/findTheOldest.js
@@ -1,5 +1,35 @@
-let findTheOldest = function() {
+// const people = [
+//    {
+//      name: 'Carly',
+//      yearOfBirth: 1942,
+//      yearOfDeath: 1970,
+//    },
+//    {
+//      name: 'Ray',
+//      yearOfBirth: 1962,
+//      yearOfDeath: 2011
+//    },
+//    {
+//      name: 'Jane',
+//      yearOfBirth: 1912,
 
-}
+//    },
+//  ]
 
+ const findTheOldest = function(array) {
+   const findOldest = array.map((person) => {
+      if (!person.yearOfDeath) {
+         person.yearOfDeath = new Date().getFullYear() 
+      }
+      person.age = person.yearOfDeath - person.yearOfBirth;
+   });
+   array.sort(function(a, b) {
+      return b.age-a.age;
+      
+   });
+   console.table(array)
+   return array[0];
+};
+
+// console.table(findTheOldest(people))
 module.exports = findTheOldest
