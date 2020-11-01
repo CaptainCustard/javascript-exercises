5adee64626caf19fc5d65ddce54a12c0979b1028
Omar Saleh <omarsaleh92@gmail.com>
2020-11-01 12:39:01 -0600

    Added solution and html page for Chrome debugging

--- 12_getTheTitles/getTheTitles.js
+++ 12_getTheTitles/getTheTitles.js
@@ -1,5 +1,19 @@
-const getTheTitles = function() {
-
+// const books = [
+//    {
+//      title: 'Book',
+//      author: 'Name'
+//    },
+//    {
+//      title: 'Book2',
+//      author: 'Name2'
+//    },
+//    {
+//       title: undefined,
+//       author: 'Name2'
+//     }
+//  ]
+const getTheTitles = function(array) {
+   return array.map(book => book.title) 
 }
-
+// console.table(getTheTitles(books))
 module.exports = getTheTitles;
