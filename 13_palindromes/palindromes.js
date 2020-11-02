475808031c022753f703947c7055a4ace87cf201
Omar Saleh <omarsaleh92@gmail.com>
2020-11-01 20:43:07 -0600

    Added solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function() {
+// const name = 'Race car'
+
+const palindromes = function(string) {
+   processedString = string.replace(/[^A-Za-z]/g, "").toLowerCase()
+   return (
+      processedString
+                     .split('')
+                     .reverse()
+                     .join('') == processedString
+   );
+   // A longer solution:
+   // const array = [...string.replace(/[^A-Za-z]/g, "").toLowerCase()];
+   // const arrayDuplicate = [...string.replace(/[^A-Za-z]/g, "").toLowerCase()];
+   // const reversedArray = arrayDuplicate.reverse();
+   // console.log(`${array} --- ${reversedArray}`)
+   // return (array.join('') == reversedArray.join('')) ? true : false;
+   
 
 }
+// console.log(palindromes(name))
 
 module.exports = palindromes
